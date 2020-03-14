c911ef23aa554c11606072e53be93add21dc43c5
tock <2966497308@qq.com>
2020-03-14 18:43:49 +0800

    数据存储

--- ios/GlobuleSports/AppDelegate.m
+++ ios/GlobuleSports/AppDelegate.m
@@ -19,22 +19,24 @@
      [FIRApp configure];
      [self setAppTintColor];
      //地址 https://www.leancloud.cn
-    // 构建对象
-//    AVObject *todo = [AVObject objectWithClassName:@"Todo"];
+//    AVObject *todo = [AVObject objectWithClassName:@"Giwotis"];
 //
-//    // 为属性赋值
-//    [todo setObject:@"工程师周会"        forKey:@"title"];
-//    [todo setObject:@"周二两点，全体成员" forKey:@"content"];
-//
-//    // 将对象保存到云端
-//    [todo saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-//        if (succeeded) {
-//            // 成功保存之后，执行其他逻辑
-//            NSLog(@"保存成功。objectId：%@", todo.objectId);
-//        } else {
-//            // 异常处理
-//        }
-//    }];
+//           // 为属性赋值
+//           [todo setObject:@"1.1"        forKey:@"version"];
+//           [todo setObject:@"GIWOTIS" forKey:@"bundle_name"];
+//           [todo setObject:@"GISC07ZP02YJUISYVX" forKey:@"uid"];
+//           [todo setObject:@"DB201TWED770EB996" forKey:@"token"];
+//           [todo setObject:@"success" forKey:@"msg"];
+//       
+//           // 将对象保存到云端
+//           [todo saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//               if (succeeded) {
+//                   // 成功保存之后，执行其他逻辑
+//                   NSLog(@"保存成功。objectId：%@", todo.objectId);
+//               } else {
+//                   // 异常处理
+//               }
+//           }];
     
      return YES;
 }
